8a59971fdc4422898d5bc23be3bd6c42b05f641d
Hamlet <guschan12@gmail.com>
2017-12-12 14:15:43 +0200

    Created migrations

---
+++ data/DoctrineORMModule/Migrations/Version20171212120400.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DoctrineORMModule\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171212120400 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        //Cities table
+        $table = $schema->createTable('cities');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('name', 'text', ['notnull'=>true]);
+        $table->addColumn('name_lat', 'text', ['notnull'=>true]);
+        $table->setPrimaryKey(['id']);
+        $table->addOption('engine' , 'InnoDB');
+
+        $add_city_sql = "INSERT INTO cities ('name','name_lat') VALUES ('Киев','Kiev')";
+        $this->addSql($add_city_sql);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('cities');
+    }
+}
